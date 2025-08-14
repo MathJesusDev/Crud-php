d2d3706c07328bb0b206a682104fd64ff6eca432
MathJesusDev <matheus.jesus6505@gmail.com>
2025-08-14 16:45:12 -0300

    feat: Adiciona funções de CRUD e setup de database

--- Crud/delete.php
+++ Crud/delete.php
@@ -2,3 +2,41 @@
 
 // deleta um registro do banco de dados
 require_once '../Database/database.php';
+
+$title = 'Crud simples em PHP - Deletar Usuário';
+
+if ($_GET) {
+    $id = $_GET['id'];
+   if ($id){
+    $database -> delUser($user);    
+    }
+}
+
+header("Location: index.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <body class="container">
+    <title>CRUD Simples - Deletar Usuário</title>
+</head>
+
+<body class="container">
+    <h1><?php echo $title ?></h1>
+
+    <form action="delete.php" method="post">
+        <div class="mb-3">
+            <label for="id" class="form-label">ID do Usuário</label>
+            <input type="number" class="form-control" id="id" name="id" required>
+        </div>
+
+        <button type="submit" class="btn btn-danger">Deletar</button>
+        <a href="index.php" class="btn-secondary">Cancelar</button>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
+require_once "../database/database.php";
 
+$users = $database->getUsers(); // Obtém os usuários do banco de dados
+$title = "Crud simples em PHP - Usuários";
+
+use Database\database;
 ?>
 
 <!DOCTYPE html>
@@ -10,11 +15,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <body class="container">
-    <title>Document</title>
+    <title>CRUD Simples em PHP - Usuários</title>
 </head>
 
-<body>
-    <h1></h1>
-</body>
+<body class="container">
+    <h1><?php echo $title ?></h1>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Sobrenome</th>
+                <th>Email</th>
+                <th>Data de Nascimento</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($users as $user): ?>
+            <tr>
+                <td><?php echo $user['id']; ?></td>
+                <td><?php echo $user['name']; ?></td>
+                <td><?php echo $user['name_first']; ?></td>
+                <td><?php echo $user['name_last']; ?></td>
+                <td><?php echo $user['email']; ?></td>
+                <td><?php echo $user['bday']; ?></td>
+                <td>
+                    <a href="edit.php?id=<?php echo $user['id']; ?>" class="btn btn-primary">Edit</a>
+                    <a href="delete.php?id=<?php echo $user['id']; ?>" class="btn btn-danger">Delete</a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+    <a href="add.php" class="btn btn-success">Add User</a>
 
-</html>
+</body>
